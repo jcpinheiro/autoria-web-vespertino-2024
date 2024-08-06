d52a2fb9cd1caef9e1d30e3fc938a970bf8267b5
João Carlos <jcpinheiro@ifma.edu.br>
2024-08-06 17:03:14 -0300

    continuação do  projeto com javascript

---
+++ 04-javascript/javascript/vendas.js
@@ -0,0 +1,16 @@
+let nome = document.getElementById('nome').value
+let quantidade = document.querySelector('#quantidade').value
+let valor = document.querySelector('#preco').value
+
+
+document.querySelector('#btnCadastro')
+    .addEventListener('click', function() {
+        console.log(nome)
+        console.log(quantidade )
+        console.log(valor)
+
+      }) 
+
+    
+
+
