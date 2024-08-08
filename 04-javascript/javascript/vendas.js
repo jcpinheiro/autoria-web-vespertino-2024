b33401b4ff9a614034c422c56d915e92a741aafd
João Carlos <jcpinheiro@ifma.edu.br>
2024-08-08 17:57:23 -0300

    Finalização da página de pedido de vendas

--- 04-javascript/javascript/vendas.js
+++ 04-javascript/javascript/vendas.js
@@ -8,11 +8,45 @@ document.querySelector('#btnCadastro')
         
         event.preventDefault()
 
-        console.log(nome.value)
-        console.log(quantidade.value )
-        console.log(valor.value)
+        let inputs = [nome.value, quantidade.value, valor.value]
 
+        let linha = document.createElement('tr')
+
+/*    for(let i=0; i < inputs.length; i++) {
+          console.log(inputs[i] )
+        }
+ */
+/*        inputs.forEach(function(campo) {
+          console.log(campo)
+       })  */
+
+       inputs.forEach(campo => {
+          let celula = document.createElement('td')
+          celula.textContent = campo
+          linha.appendChild(celula)
+       }) 
+       let subTotal = parseFloat(inputs[1]) * parseFloat(inputs[2])
+       let celula = document.createElement('td')
+       celula.textContent = subTotal
+
+       linha.appendChild(celula )
+
+       document
+           .querySelector('#tbodyItemPedido')
+           .appendChild(linha )
+      
+       limpaFormulario();
+          
       }) 
+
+      function limpaFormulario() {
+         nome.value = ''
+         quantidade.value = ''
+         valor.value = ''
+
+         nome.focus()
+
+      }
 
     
 
